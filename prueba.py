70a0f7a789a4661891389bae09980f3017c60f1c
TeamARV <andresriveravega@msn.com>
2021-09-17 15:29:04 -0500

    resolviendo_conflicto

--- prueba.py
+++ prueba.py
@@ -2,12 +2,9 @@ print("Hola Mundo.")
 print("Buenas Tardes compañeros")
 print("Probando probando........")
 print("Compañeros recuerden Probar GIT")
-<<<<<<< HEAD
 A=[1,2,3,4,10]
 for i in (A):
     print (i)
 
 print("Disculpas por la demora, aquí mi pequeño mensaje")
-=======
 print("Probando")
->>>>>>> Development
