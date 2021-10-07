6f7fa9925dc15156d41cdaa9768210f80577417e
TeamARV <andresriveravega@msn.com>
2021-10-07 09:42:07 -0500

    crud parcial + busqueda en inventarios

--- src/pages/Page6.jsx
+++ src/pages/Page6.jsx
@@ -20,14 +20,18 @@ function Page6() {
       { idProducto: 3, descripcionProducto: "Classic Pizza", valorProducto: 30000, inventarioProducto: 10 },
       { idProducto: 4, descripcionProducto: "Mega Pizza", valorProducto: 50000, inventarioProducto: 10 },
       { idProducto: 5, descripcionProducto: "Nerd Pizza", valorProducto: 60000, inventarioProducto: 10 },
+      
     ]
 
 
     const [Productos,setProductos] = useState([])
-    const [Id,setId] = useState(0);
-    const [Descripcion,setDescripcion] = useState("nada");
-    const [Precio,setprecio] = useState(0);
-    const [Busqueda,setBusqueda] = useState("Busqueda invetario");
+    const [Id,setId] = useState();
+    const [Descripcion,setDescripcion] = useState("");
+    const [Precio,setprecio] = useState();
+    const [Cantidad,setcantidad] = useState();
+    const [Busqueda,setBusqueda] = useState("");
+    const [FiltroProductos,setFiltroProductos] = useState(Productos);
+    
 
     useEffect(()=>{
         //Aqui voy a jalar info de la base datos
@@ -36,9 +40,31 @@ function Page6() {
     },[]);
 
 
+    const enviarBaseDatos =()=> {
+      console.log(`enviado back , ${Id}, ${Descripcion}, ${Precio} , ${Cantidad}`)   
+      setProductos([...Productos,{idProducto:Id, descripcionProducto:Descripcion,valorProducto:Precio,inventarioProducto:Cantidad}]); 
+
+    }
+
+    useEffect(()=> {
+      console.log("busqueda" , Busqueda);
+
+      setFiltroProductos(
+  
+       Productos.filter((x) => {
+        return JSON.stringify(x).toLocaleLowerCase().includes(Busqueda.toLocaleLowerCase());
+      })
+
+       
+
+      )
+
+ 
+    },[Busqueda,Productos]);
 
 
-    const Mytabladeproductos = ({ ListaProductos }) =>{
+
+    const Mytabladeproductos = ({ ListaProductos} ) =>{
         
 
         useEffect(()=> {
@@ -93,11 +119,11 @@ function Page6() {
 
         
         <br />
-        <input class="input2"  onChange ={(e)=>{setBusqueda(e.target.value);console.log(e.target.value)}} type="text" name="search" placeholder={Busqueda}/>
+        <input class="input2"  onChange ={(e)=>{setBusqueda(e.target.value);console.log(e.target.value) }} type="text" name="search" placeholder={Busqueda}/>
         <br /> <br />
 
         
-        <Mytabladeproductos ListaProductos={Productos}/>
+        <Mytabladeproductos ListaProductos={FiltroProductos} />
         
         
         {/* <BaseDatosPruebas funcion="buscar" descripcion="Mini Pizza"/> */}
@@ -113,10 +139,11 @@ function Page6() {
         {/* <BaseDatosPruebas funcion="actualizar" id ="0" descripcion=" esto es una update al inventario"/> */}
 
         <form action="formulario actualizar" method="get">
-        <input type="text" onChange ={(e)=>{setId(e.target.value)}} placeholder="ID #️⃣" />
-        <input type="text" onChange ={(e)=>{setDescripcion(e.target.value)}}placeholder="Descripcion 🎫" />
-        <input type="text" onChange ={(e)=>{setprecio(e.target.value)}}placeholder="Precio 💲" />
-        <input type="button" onClick={(e)=>{ console.log(" | La variable Id = ",Id + " | La variable Descripcion = ",Descripcion + " | La variable precio= " , 
+        <input type="text" value={Id} onChange ={(e)=>{setId(e.target.value)}} placeholder="ID #️⃣" />
+        <input type="text" value={Descripcion} onChange ={(e)=>{setDescripcion(e.target.value)}}placeholder="Descripcion 🎫" />
+        <input type="text" value={Precio} onChange ={(e)=>{setprecio(e.target.value)}}placeholder="Precio 💲" />
+        <input type="text" value={Cantidad} onChange ={(e)=>{setcantidad(e.target.value)}}placeholder="Cantidad #" />
+        <input type="button" onClick={(e)=>{ enviarBaseDatos() ;console.log(" | La variable Id = ",Id + " | La variable Descripcion = ",Descripcion + " | La variable precio= " , 
         Precio + " | El search es = " ,Busqueda  )}}  value="Actualizar 🔄" />
         <input type="reset" value="Borrar ❌"></input>
         </form>
